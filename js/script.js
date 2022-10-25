d6596639c1af78cc0d1143237eabda246d95f591
Cyprian Adamski <cyprian.adamski@gmail.com>
2022-10-25 13:32:37 +0200

    add 'data-tags' to html, add generateTags fnction (done: find all articles,START LOOP: for every art.., find tags wrapper,make html variable, get tags from data-tags attribiute)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/* eslint-disable indent */
 'use strict';
 
 const titleClickHandler = function (event) {
@@ -44,7 +45,8 @@ const titleClickHandler = function (event) {
 };
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
-  optTitleListSelector = '.titles';
+  optTitleListSelector = '.titles',
+  optArticleTagsSelector = '.post-tags .list';
 
 function generateTitleLinks() {
   /* [DONE] remove contents of titleList */
@@ -79,7 +81,7 @@ function generateTitleLinks() {
       '"><span>' +
       articleTitle +
       '</span></a></li>';
-    console.log('linkHtml:', html);
+    //console.log('linkHtml:', html);
 
     /* insert link into titleList */
 
@@ -95,3 +97,31 @@ function generateTitleLinks() {
 }
 
 generateTitleLinks();
+
+function generateTags() {
+  /* find all articles */
+
+  const articles = document.querySelectorAll(optArticleSelector);
+
+  /* START LOOP: for every article: */
+
+  for (let article of articles) {
+    /* find tags wrapper */
+    const titleList = article.querySelector(optArticleTagsSelector);
+    //console.log('remove:', titleList);
+    /* make html variable with empty string */
+    let html = '';
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute('data-tags');
+    console.log('data-tags:', articleTags);
+    /* split tags into array */
+    /* START LOOP: for each tag */
+    /* generate HTML of the link */
+    /* add generated code to html variable */
+    /* END LOOP: for each tag */
+    /* insert HTML of all the links into the tags wrapper */
+    /* END LOOP: for every article: */
+  }
+}
+
+generateTags();
